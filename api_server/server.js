46227384b4bdf2e8ea429ac90518c6481de73d53
Claudia Cuevas <omordn@gmail.com>
2016-06-18 11:33:16 -0700

    add ember app this is only the beginning of the app configuration. this commit includes a working POST and GET request from ember-data (payload) to the API.

--- api_server/server.js
+++ api_server/server.js
@@ -12,6 +12,14 @@ mongoose.connect('mongodb://ember:soccer15@ds031183.mlab.com:31183/freezr_dev');
 
 let Item = require('./models/item');
 
+// Configure the app to use the below headers/ access
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  next();
+});
+
 // Configure the app to use bodyParser()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -28,22 +36,24 @@ router.route('/items')
   // POST a new food item
   .post((req, res) => {
     let item = new Item();
-    item.author = req.body.author;
-    item.food = req.body.food;
+
+    item.author = req.body.item.author;
+    item.food = req.body.item.food;
 
     // save the new item and check for errors
     item.save((e) => {
       if (e) { res.send(e); }
 
-      res.json({ message: 'A new item was created!' });
+      res.json({ item: item });
     });
   })
   // GET all food items
   .get((req, res) => {
-    Item.find((e, items) => {
-      if (e) { res.send(e); }
 
-      res.json(items);
+    Item.find((e, items) => {
+      if (e) { res.send({ error: e }); }
+
+      res.json({ items: items });
     });
   });
 
@@ -55,7 +65,7 @@ router.route('/items/:id')
     Item.findById(req.params.id, (e, item) => {
       if (e) { res.send(e); }
 
-      res.json(item);
+      res.json({ item: item });
     });
   })
   // Update (i.e. PUT) a food item
@@ -70,7 +80,7 @@ router.route('/items/:id')
       item.save((e) => {
         if (e) { res.send(e); }
 
-        res.json({ message: 'Item was updated successfully!' });
+        res.json({ item: item });
       });
     });
   })
@@ -81,7 +91,7 @@ router.route('/items/:id')
     }, (e, item) => {
       if (e) { res.send(e); }
 
-      res.json({ message: 'Item was successfully deleted!' });
+      res.json({});
     });
   });
 
